0d2a4a76f8e4c29520369b0322e235098145c03a
timealtair <83885014+timealtair@users.noreply.github.com>
2025-07-08 19:57:06 +0300

    added 'networkmanager' install & setup

--- system_setup.sh
+++ system_setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/sh
 pacman -Syu
-pacman -S terminus-font konsole vivaldi vim fish clipcat i3-wm polybar flameshot physlock solaar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulsemixer brightnessctl playerctl adobe-source-code-pro-fonts rofi rofimoji nemo vlc bluez bluez-utils 
+pacman -S terminus-font konsole vivaldi vim fish clipcat i3-wm polybar flameshot physlock solaar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulsemixer brightnessctl playerctl adobe-source-code-pro-fonts rofi rofimoji nemo vlc bluez bluez-utils networkmanager network-manager-applet networkmanager-openvpn
 
 cp ./configs/services/* /etc/systemd/system/
 systemctl enable --now solaar-fn-swap-off.service
 systemctl enable --now bluetooth.service
+systemctl enable --now NetworkManager.service
