26ddcfbc52fdb3cdd192dff798848597c3405614
timealtair <83885014+timealtair@users.noreply.github.com>
2025-07-08 19:57:32 +0300

    added 'git' install

--- system_setup.sh
+++ system_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 pacman -Syu
-pacman -S terminus-font konsole vivaldi vim fish clipcat i3-wm polybar flameshot physlock solaar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulsemixer brightnessctl playerctl adobe-source-code-pro-fonts rofi rofimoji nemo vlc bluez bluez-utils networkmanager network-manager-applet networkmanager-openvpn
+pacman -S terminus-font konsole vivaldi vim fish clipcat i3-wm polybar flameshot physlock solaar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulsemixer brightnessctl playerctl adobe-source-code-pro-fonts rofi rofimoji nemo vlc bluez bluez-utils networkmanager network-manager-applet networkmanager-openvpn git
 
 cp ./configs/services/* /etc/systemd/system/
 systemctl enable --now solaar-fn-swap-off.service
