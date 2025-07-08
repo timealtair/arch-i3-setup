fadf67ea52605c77fdb701aa7a5e2f7f08abc4fb
timealtair <83885014+timealtair@users.noreply.github.com>
2025-07-08 22:17:56 +0300

    removed 'physlock' added 'xsecurelock'

--- system_setup.sh
+++ system_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 pacman -Syu
-pacman -S terminus-font konsole vivaldi gvim fish clipcat i3-wm polybar flameshot physlock solaar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulsemixer brightnessctl playerctl adobe-source-code-pro-fonts rofi rofimoji nemo vlc bluez bluez-utils networkmanager network-manager-applet networkmanager-openvpn git ipython uv zoxide fzf xfce4-notifyd
+pacman -S terminus-font konsole vivaldi gvim fish clipcat i3-wm polybar flameshot xsecurelock solaar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulsemixer brightnessctl playerctl adobe-source-code-pro-fonts rofi rofimoji nemo vlc bluez bluez-utils networkmanager network-manager-applet networkmanager-openvpn git ipython uv zoxide fzf xfce4-notifyd
 
 cp ./configs/services/* /etc/systemd/system/
 systemctl enable --now solaar-fn-swap-off.service
