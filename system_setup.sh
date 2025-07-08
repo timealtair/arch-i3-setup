144ca3c42850ef93480904bb5b9e451cd4765d3a
timealtair <83885014+timealtair@users.noreply.github.com>
2025-07-08 19:37:59 +0300

    added 'konsole vivaldi' install & set 'vivaldi' as default browser

--- system_setup.sh
+++ system_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 pacman -Syu
-pacman -S terminus-font vim fish clipcat i3-wm polybar flameshot physlock solaar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulsemixer brightnessctl playerctl
+pacman -S terminus-font konsole vivaldi vim fish clipcat i3-wm polybar flameshot physlock solaar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulsemixer brightnessctl playerctl
 
 cp ./configs/services/* /etc/systemd/system/
 systemctl enable --now solaar-fn-swap-off.service
