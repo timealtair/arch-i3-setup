f6733000fd3451afa7576b086b4aa24687e82b24
timealtair <83885014+timealtair@users.noreply.github.com>
2025-07-08 19:27:01 +0300

    added 'solaar' service setup

--- system_setup.sh
+++ system_setup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/sh
 pacman -Syu
-pacman -S fish clipcat i3-wm polybar flameshot physlock
+pacman -S fish clipcat i3-wm polybar flameshot physlock solaar
+
+cp ./configs/services/* /etc/systemd/system/
+systemctl enable --now solaar-fn-swap-off.service
