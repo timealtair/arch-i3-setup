8f34f47266cbecbe4e7422e3276b7eb7904c4bcd
timealtair <83885014+timealtair@users.noreply.github.com>
2025-07-08 23:01:45 +0300

    added 'clipcat' setup

--- user_setup.sh
+++ user_setup.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/sh
 cp -r ./configs/dotfiles/.* $HOME/
+
+mkdir -p                       $HOME/.config/clipcat
+clipcatd default-config      > $HOME/.config/clipcat/clipcatd.toml
+clipcatctl default-config    > $HOME/.config/clipcat/clipcatctl.toml
+clipcat-menu default-config  > $HOME/.config/clipcat/clipcat-menu.toml
